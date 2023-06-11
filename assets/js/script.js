3c32c5893c7f7d15497c123b22a9d87a8abce707
kunal-gulati-dev <knlgulati365@gmail.com>
2023-06-11 15:49:05 +0530

    added the hero section and after that html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,49 +5,35 @@ window.addEventListener("load", () => {
 })
 
 
-
-
-
-
-// const preloader = document.querySelector("[data-preloader]");
-
-// window.addEventListener('load', () => {
-//     preloader.classList.add("remove")
-// })
-
-
 // add event on multiple elements
 
-// const addEventOnElements = function (elements, eventType, callback) {
-//     for (let i = 0; i < elements.length; i++){
-//         elements[i].addEventListener(eventType, callback);
-//     }
-// }
-
+const addEventOnElements = function(elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i< len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
 
 // Navbar toggler for mobile
 
-// const navbar = document.querySelector('[data-navbar]');
-// const navTogglers = document.querySelectorAll("[data-nav-toggler]")
-// const overlay = document.querySelector("[data-overlay]")
+const navbar = document.querySelector("[data-navbar]")
 
-// const toggleNav = function () {
-//     navbar.classList.toggle("active")
-//     overlay.classList.toggle("active")
-//     document.body.classList.toggle("nav-active")
-// }
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]")
 
+const toggleNav = function () {
+    navbar.classList.toggle("active")
+    overlay.classList.toggle("active")
+    document.body.classList.toggle("nav-active")
+}
 
-// addEventOnElements(navTogglers, "click", toggleNav);
+addEventOnElements(navTogglers, "click", toggleNav);
+
 
 // header
 
-// const header = document.querySelector("[data-header]");
+const header = document.querySelector("[data-header]");
 
-
-// window.addEventListener("scroll", function() {
-
-//     header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
-
-
-// })
+window.addEventListener("scroll", function () {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+    
+})
